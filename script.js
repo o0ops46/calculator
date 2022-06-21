0c5855476ca4cc715bb6707a433364ec1848ac6f
Dumitru Marian <mariantdm@icloud.com>
2022-06-21 15:47:03 +0100

    created all numbers and selected the operators

--- script.js
+++ script.js
@@ -2,14 +2,25 @@
 //select elements.
 const clearBtn = document.querySelector(".clearBtn");
 const inputBox = document.querySelector(".box1");
-
-clearBtn.addEventListener("click", function () {
-  inputBox.textContent = "";
-});
+const numberOne = document.querySelector(".one");
+const numberTwo = document.querySelector(".two");
+const numberThree = document.querySelector(".three");
+const numberFour = document.querySelector(".four");
+const numberFive = document.querySelector(".five");
+const numberSix = document.querySelector(".six");
+const numberSeven = document.querySelector(".seven");
+const numberEight = document.querySelector(".eight");
+const numberNine = document.querySelector(".nine");
 
 const add = (a, b) => {
   return a + b;
 };
 const subtract = (a, b) => {
-  return a;
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
+const divide = (a, b) => {
+  return a / b;
 };
